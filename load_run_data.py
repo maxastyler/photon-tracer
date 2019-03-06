64c51784b88d3fb1e0de7e15332a312ef95d1202
Max Tyler <maxastyler@gmail.com>
2019-03-06 16:38:02 +0000

    Make histogram of data

--- load_run_data.py
+++ load_run_data.py
@@ -1,21 +1,29 @@
 import numpy as np
+from numpy import linalg as LA
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from io import StringIO
 
 paths = []
 
-with open('./run_data.txt') as f:
-    for line in f.readlines()[0:1000]:
+with open('/home/max/git/photon_tracer/data/photon_paths.txt') as f:
+    for line in f.readlines():
         vec = np.loadtxt(StringIO(line))
-        paths.append(vec.reshape((int(len(vec)/3), 3)))
+        vec = vec.reshape((int(len(vec)/3), 3))
+        paths.append(vec)
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_xlim3d(0,1)
-ax.set_ylim3d(0,1)
-ax.set_zlim3d(0,1)
-path_num = 1
-for path_num in range(len(paths)):
-    ax.plot(paths[path_num][:, 0], paths[path_num][:, 1], paths[path_num][:, 2])
+differences = []
+for path in paths:
+    differences.append(path[1:]-path[:-1])
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# ax.set_xlim3d(0,1)
+# ax.set_ylim3d(0,1)
+# ax.set_zlim3d(0,1)
+path_lengths = np.array([LA.norm(i, axis=1).sum() for i in differences])
+plt.hist(path_lengths, bins=100)
+# print(LA.norm(differences[0], axis=1).sum())
+# for diff in differences:
+#     ax.plot(diff[:, 0], diff[:, 1], diff[:, 2])
 plt.show()
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ const PHOTON_DIRECTION: (f32, f32, f32) = (0.0, 0.0, 1.0);
 const PHOTON_NUM_DEFAULT: usize = 1000;
 const CORE_NUM_DEFAULT: usize = 1;
 
-const PARTICLE_RADIUS: f32 = 0.000_000_001;
+const PARTICLE_RADIUS: f32 = 0.00001;
 
 const STEP_SIZE: f32 = 0.000_01;
 
